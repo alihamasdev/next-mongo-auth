7ed23a25897b551a408fb5799f988edfe6bfc6c5
Ali Hamas <alihamasdev@gmail.com>
2024-10-31 19:55:37 +0500

    complete login flow

---
+++ src/app/(public-routes)/login/action.ts
@@ -0,0 +1,25 @@
+"use server";
+import bcrypt from "bcryptjs";
+import { dbConnect } from "@/db/connect";
+import { redirect } from "next/navigation";
+import { loginSchema } from "@/lib/schema";
+import { createSession } from "@/lib/session";
+import User, { type UserSchema } from "@/db/models/user-model";
+
+dbConnect();
+
+export default async function action(prevState: any, formData: FormData) {
+	const validation = loginSchema.safeParse(Object.fromEntries(formData));
+	if (!validation.success) return { errors: validation.error.flatten().fieldErrors };
+
+	const { email, password } = validation.data;
+
+	const user: UserSchema | null = await User.findOne({ email });
+	if (!user) return { errors: { email: ["User with email doesn't exists"] } };
+
+	const validPassword = await bcrypt.compare(password, user.password);
+	if (!validPassword) return { errors: { password: ["Please enter valid password"] } };
+
+	const session = await createSession({ userId: user._id });
+	redirect("/profile");
+}
--- src/app/(public-routes)/login/page.tsx
+++ src/app/(public-routes)/login/page.tsx
@@ -1,24 +1,33 @@
-import { Fragment } from "react";
+"use client";
+import Form from "next/form";
+import signUpAction from "./action";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { Fragment, useActionState } from "react";
 
 export default function Page() {
+	const [state, action, pending] = useActionState(signUpAction, undefined);
+
 	return (
 		<Fragment>
 			<title>Login | Next.js Authentication</title>
 			<h1 className="text-center text-2xl font-extrabold md:text-3xl">Login</h1>
-			<form className="mt-6 flex w-full flex-col gap-y-5">
+			<Form action={action} className="mt-6 flex w-full flex-col gap-y-6">
 				<div className="relative">
 					<Label htmlFor="email">Enter email address</Label>
-					<Input name="email" type="email" id="email" />
+					<Input name="email" type="email" id="email" disabled={pending} />
+					{state?.errors?.email && <p className="error">{state.errors.email}</p>}
 				</div>
 				<div className="relative">
 					<Label htmlFor="password">Enter password</Label>
-					<Input name="password" type="password" id="password" />
+					<Input name="password" type="password" id="password" disabled={pending} />
+					{state?.errors?.password && <p className="error">{state.errors.password}</p>}
 				</div>
-				<Button className="mt-2">Login</Button>
-			</form>
+				<Button className="mt-2" disabled={pending}>
+					Login
+				</Button>
+			</Form>
 		</Fragment>
 	);
 }
