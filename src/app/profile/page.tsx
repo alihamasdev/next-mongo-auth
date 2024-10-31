56165f7a4b926c2ba113d60b02c0a4336a46d205
Ali Hamas <alihamasdev@gmail.com>
2024-11-01 00:56:09 +0500

    add profile page contents

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-	return <div>Page</div>;
+import { dbConnect } from "@/db/connect";
+import { notFound } from "next/navigation";
+import { getSessionUserId } from "@/lib/auth";
+import User, { type UserSchema } from "@/db/models/user-model";
+
+dbConnect();
+
+export default async function Page() {
+	const userId = await getSessionUserId();
+	const user: UserSchema | null = await User.findById(userId);
+
+	if (!user) return notFound();
+
+	return (
+		<section className="space-y-5">
+			<div>
+				<p>Name: {user.name}</p>
+				<p>Email: {user.email}</p>
+			</div>
+		</section>
+	);
 }
